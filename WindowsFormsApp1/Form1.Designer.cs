2d7944d5257328ecc8ca075558c8a9f378f5982a
vrmac <vrmac@NOTEDOVINI>
2022-10-03 21:54:45 -0300

    Adicionar arquivos de projeto.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cboPagamento = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtValorGasto = new System.Windows.Forms.TextBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtParcela = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // cboPagamento
+            // 
+            this.cboPagamento.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboPagamento.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.cboPagamento.FormattingEnabled = true;
+            this.cboPagamento.Location = new System.Drawing.Point(94, 111);
+            this.cboPagamento.Name = "cboPagamento";
+            this.cboPagamento.Size = new System.Drawing.Size(231, 28);
+            this.cboPagamento.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.label1.Location = new System.Drawing.Point(91, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Valor gasto:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtValorGasto
+            // 
+            this.txtValorGasto.Location = new System.Drawing.Point(184, 37);
+            this.txtValorGasto.Name = "txtValorGasto";
+            this.txtValorGasto.Size = new System.Drawing.Size(165, 20);
+            this.txtValorGasto.TabIndex = 0;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.btnCalcular.Location = new System.Drawing.Point(94, 156);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(231, 50);
+            this.btnCalcular.TabIndex = 2;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.label2.Location = new System.Drawing.Point(136, 91);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(143, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Forma de pagamento";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.label3.Location = new System.Drawing.Point(67, 254);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(116, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Valor da parcela:";
+            // 
+            // txtParcela
+            // 
+            this.txtParcela.Location = new System.Drawing.Point(184, 254);
+            this.txtParcela.Name = "txtParcela";
+            this.txtParcela.Size = new System.Drawing.Size(165, 20);
+            this.txtParcela.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(421, 321);
+            this.Controls.Add(this.txtParcela);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.txtValorGasto);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cboPagamento);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cboPagamento;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtValorGasto;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtParcela;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cboPagamento.Items.Add("A vista");
+            cboPagamento.Items.Add("2x");
+            cboPagamento.Items.Add("3x");
+            cboPagamento.Items.Add("4x");
+            cboPagamento.Items.Add("5x");
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            float vg, vp;
+            vg = float.Parse(txtValorGasto.Text);
+            if(cboPagamento.Text=="A vista")
+            {
+                vp = vg;
+            }
+            else if (cboPagamento.Text == "2x")
+            {
+                vp = vg / 2;
+            }
+            else if (cboPagamento.Text == "3x")
+            {
+                vp = vg / 3;
+            }
+            else if (cboPagamento.Text == "4x")
+            {
+                vp = vg / 4;
+            }
+            else
+            {
+                vp = vg / 5;
+            }
+            txtParcela.Text = vp.ToString();
+        }
+    }
+}
